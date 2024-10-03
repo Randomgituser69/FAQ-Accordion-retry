68fe590ae08fdb809a5d33702f8cd9e4a9e97975
Randomgituser69 <159695185+Randomgituser69@users.noreply.github.com>
2024-10-03 04:45:23 +0000

    c c

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const answerBtn = document.querySelectorAll('button')
 
+answerBtn.forEach(function(button) {
+    button.addEventListener('click', function(e) {
+      const answer = e.currentTarget.parentElement.parentElement
+      answer.classList.toggle('show-answer')
+    })
+})
